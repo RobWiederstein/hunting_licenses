cc8300b41e2d1255cf026e301e65c1152c049e9b
Rob Wiederstein <khuon68@gmail.com>
2020-11-25 16:12:36 -0600

    format choropleth

--- plot_choropleth_map.R
+++ plot_choropleth_map.R
@@ -1,5 +1,6 @@
 # load geospatial libraries
-x <- c("ggmap", "rgdal", "rgeos", "maptools", "dplyr", "tidyr", "tmap", "sf")
+x <- c("ggmap", "rgdal", "rgeos", "maptools", "dplyr", "tidyr", "tmap", "sf",
+       "mapproj")
 # install.packages(x)
 lapply(x, library, character.only = TRUE)
 # load data
@@ -15,93 +16,65 @@ df.03 <- tidyr::spread(df.02, year, value)
 #add column pct change in per capita
 df.03$pct_change <- (df.03$'2020' - df.03$'1960') / df.03$'1960'
 df.03$pct_change <- round(df.03$pct_change * 100, 1)
-#septiles
-df.03$septile <- ggplot2::cut_interval(df.03$pct_change, n = 7,
-                                      labels = paste("S", 1:7, sep = "")
-)
-
-
 # add polygons
 dsn <- "./data_pure/shp/cb_2019_us_all_20m/cb_2019_us_state_20m"
 sl.00 <- rgdal::readOGR(dsn = dsn, layer = "cb_2019_us_state_20m", stringsAsFactors = F)
 #omit DC & PR
 sl.01 <- sl.00[-grep("DC|PR", sl.00$STUSPS), ]
+# #bring alaska and hawaii to the US
+sl.02 <- spTransform(sl.01, CRS("+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"))
+sl.02@data$id = rownames(sl.02@data)
+alaska = sl.02[sl.02$STATEFP=="02",]
+alaska = elide(alaska, rotate=-50)
+alaska = elide(alaska, scale=max(apply(bbox(alaska), 1, diff)) / 2.3)
+alaska = elide(alaska, shift=c(-2100000, -2500000))
+proj4string(alaska) = proj4string(sl.02)
 
-# keep county name
-keep.col <- c(1, 3, 5)
-cl.01 <- cl.00[, keep.col]
-colnames(cl.01@data) <- tolower(colnames(cl.01@data))
-colnames(cl.01@data)[3] <- "location"
-ogrDrivers()
-dsn <- system.file("vectors", package = "rgdal")[1]
-ogrListLayers(dsn)
-ogrInfo(dsn)
-ogrInfo(dsn=dsn, layer="cities")
-owd <- getwd()
-setwd(dsn)
-ogrInfo(dsn="cities.shp")
-ogrInfo(dsn="cities.shp", layer="cities")
-setwd(owd)
-ow <- options("warn")$warn
-options("warn"=1)
-cities <- readOGR(dsn=dsn, layer="cities")
-str(slot(cities, "data"))
-cities$POPULATION <- type.convert(as.character(cities$POPULATION),
-                                  na.strings="-99", numerals="no.loss")
-str(slot(cities, "data"))
-cities <- readOGR(dsn=dsn, layer="cities", GDAL1_integer64_policy=TRUE)
-str(slot(cities, "data"))
-options("warn"=ow)
-summary(cities)
-table(Encoding(as.character(cities$NAME)))
-ogrInfo(dsn=dsn, layer="kiritimati_primary_roads")
-OGRSpatialRef(dsn=dsn, layer="kiritimati_primary_roads")
-kiritimati_primary_roads <- readOGR(dsn=dsn, layer="kiritimati_primary_roads")
-summary(kiritimati_primary_roads)
-ogrInfo(dsn=dsn, layer="scot_BNG")
-OGRSpatialRef(dsn=dsn, layer="scot_BNG")
-scot_BNG <- readOGR(dsn=dsn, layer="scot_BNG")
-summary(scot_BNG)
-if ("GML" %in% ogrDrivers()$name) {
-        dsn <- system.file("vectors/airports.gml", package = "rgdal")[1]
-        airports <- try(readOGR(dsn=dsn, layer="airports"))
-        if (!inherits(airports, "try-error")) summary(airports)
-}
-dsn <- system.file("vectors/ps_cant_31.MIF", package = "rgdal")[1]
-ogrInfo(dsn=dsn, layer="ps_cant_31")
-ps_cant_31 <- readOGR(dsn=dsn, layer="ps_cant_31")
-summary(ps_cant_31)
-sapply(as(ps_cant_31, "data.frame"), class)
-ps_cant_31 <- readOGR(dsn=dsn, layer="ps_cant_31", stringsAsFactors=FALSE)
-summary(ps_cant_31)
-sapply(as(ps_cant_31, "data.frame"), class)
-dsn <- system.file("vectors/Up.tab", package = "rgdal")[1]
-ogrInfo(dsn=dsn, layer="Up")
-Up <- readOGR(dsn=dsn, layer="Up")
-summary(Up)
-dsn <- system.file("vectors/test_trk2.gpx", package = "rgdal")[1]
-test_trk2 <- try(readOGR(dsn=dsn, layer="tracks"))
-if (!inherits(test_trk2, "try-error")) summary(test_trk2)
-test_trk2pts <- try(readOGR(dsn=dsn, layer="track_points"))
-if (!inherits(test_trk2pts, "try-error")) summary(test_trk2pts)
-dsn <- system.file("vectors", package = "rgdal")[1]
-ogrInfo(dsn=dsn, layer="trin_inca_pl03")
-birds <- readOGR(dsn=dsn, layer="trin_inca_pl03")
-summary(birds)
-dsn <- system.file("vectors/PacoursIKA2.TAB", package = "rgdal")[1]
-try(ogrInfo(dsn, "PacoursIKA2"))
-ogrInfo(dsn, "PacoursIKA2", require_geomType="wkbPoint")
-plot(readOGR(dsn, "PacoursIKA2", require_geomType="wkbLineString"), col="red")
-plot(readOGR(dsn, "PacoursIKA2", require_geomType="wkbPoint"), add=TRUE)
-odir <- getwd()
-setwd(system.file("vectors", package = "rgdal")[1])
-ow <- options("warn")$warn
-options("warn"=1)
-ogrInfo("test64.vrt", "test64")
-str(readOGR("test64.vrt", "test64", verbose=FALSE, integer64="allow.loss")$val)
-str(readOGR("test64.vrt", "test64", verbose=FALSE, integer64="warn.loss")$val)
-str(readOGR("test64.vrt", "test64", verbose=FALSE, integer64="no.loss")$val)
-str(readOGR("test64.vrt", "test64", verbose=FALSE, stringsAsFactors=FALSE,
-            integer64="no.loss")$val)
-setwd(odir)
-options("warn"=ow)
+hawaii = sl.02[sl.02$STATEFP=="15",]
+hawaii = elide(hawaii, rotate=-35)
+hawaii = elide(hawaii, shift=c(5400000, -1400000))
+proj4string(hawaii) = proj4string(sl.02)
+
+#
+sl.02 = sl.02[!sl.02$STATEFP %in% c("02", "15"), ]
+sl.02 = rbind(sl.02, alaska, hawaii)
+
+#join sp with dataframe
+#http://www.nickeubank.com/wp-content/uploads/2015/10/RGIS2_MergingSpatialData_part1_Joins.html
+sf.00 <- merge(sl.02, df.03, by.x = "STATEFP", by.y = "fips")
+class(sf.00)
+
+us_states <- sf.00
+tmap_mode("plot")
+
+us_states_map = 
+      tm_shape(us_states) + 
+      tm_polygons("pct_change",
+                  midpoint = NA,
+                  palette = "RdYlGn", 
+                  n = 7, 
+                  contrast = c(0, 0.8)
+                  ) + 
+      tm_layout(main.title = "US Pct Change in Hunters Per Capita \n 1960 - 2020",
+                main.title.size = 1,
+                main.title.position = "center",
+                bg.color = "white",
+                frame = FALSE,
+                legend.text.color = "black",
+                legend.position = c(.9, 0),
+                legend.outside = FALSE
+                ) + 
+      tm_text("abb", size = .5)
+
+us_states_map
+# hawaii <- sf.00[sf.00$NAME == "Hawaii", ]
+# hawaii_map = tm_shape(hawaii) + tm_polygons("pct_change") + 
+#       tm_layout(title = "Hawaii", frame = FALSE, bg.color = NA, 
+#                 title.position = c("LEFT", "BOTTOM"))
+# alaska <- sf.00[sf.00$NAME == "Alaska", ]
+# alaska_map = tm_shape(alaska) + tm_polygons() + 
+#       tm_layout(title = "Alaska", frame = FALSE, bg.color = NA)
+      
+
+# print(hawaii_map, vp = grid::viewport(0.35, 0.1, width = 0.2, height = 0.1))
+# print(alaska_map, vp = grid::viewport(0.15, 0.15, width = 0.3, height = 0.3))
