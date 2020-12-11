26949424b14f13122864a4a01ca1dd75aaa76863
Rob Wiederstein <khuon68@gmail.com>
2020-12-11 06:35:57 -0600

    build out table for F&W

--- R/build_table_for_state_f_and_w.R
+++ R/build_table_for_state_f_and_w.R
@@ -15,8 +15,15 @@ df.pchl <-
       summarize(hunters_per_1000_2020 = value *1000, .groups = "keep")
 dt <- merge(dt, df.pchl)
 dt$hunters_per_1000_2020 <-round(dt$hunters_per_1000_2020, 1)
-
-#
+#wr_apportionment dollars per hunter
+df.wp <- 
+      df.00 %>%
+      dplyr::filter(key == "wr_apportionment_real_dollars" & year == 2020)
+df.hl <-
+      df.00 %>%
+      dplyr::filter(key == "certified_paid_hunting_license_holders"  & year == 2020)
+df.pcwp <- left_join(df.wp, df.hl)
+#ugh
 
 file <- "./data_tidy/appendix_state_fish_wildlife_metrics.csv"
 write.csv(dt, file = file, row.names = F)
